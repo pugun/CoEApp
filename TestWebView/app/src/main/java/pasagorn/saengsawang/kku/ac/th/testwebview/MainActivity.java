40fc775f3dcec2bf96ac64fc60a9544044b8b7ed
Pasagorn <pasagorn_pg@kkumail.com>
2019-05-01 13:33:38 +0700

    test prototype webview

---
+++ TestWebView/app/src/main/java/pasagorn/saengsawang/kku/ac/th/testwebview/MainActivity.java
@@ -0,0 +1,50 @@
+package pasagorn.saengsawang.kku.ac.th.testwebview;
+import android.app.Activity;
+import android.net.http.SslError;
+import android.os.Bundle;
+import android.webkit.GeolocationPermissions;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    // here you can put your url address
+    // @BindView(R.id.webView)
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = (WebView)findViewById(R.id.webView);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setAppCacheEnabled(true);
+        webView.getSettings().setDatabaseEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setSupportZoom(true);
+        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+        //webView.setWebViewClient(new GeoWebViewClient());
+        // Below required for geolocation
+
+        webView.getSettings().setGeolocationEnabled(true);
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                handler.proceed();
+            }
+        });
+        webView.loadUrl("https://165.22.240.130");
+        webView.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
+                // callback.invoke(String origin, boolean allow, boolean remember);
+                callback.invoke(origin, true, false);
+
+            }
+        });
+
+    }
+}
