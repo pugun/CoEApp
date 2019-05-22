3d3cad642975936683c0b1e7686506cfdb4cc6a9
Pasagorn <pasagorn_pg@kkumail.com>
2019-05-22 23:37:36 +0700

    add Splash Screen

--- TestWebView/app/src/main/java/pasagorn/saengsawang/kku/ac/th/testwebview/MainActivity.java
+++ TestWebView/app/src/main/java/pasagorn/saengsawang/kku/ac/th/testwebview/MainActivity.java
@@ -1,7 +1,12 @@
 package pasagorn.saengsawang.kku.ac.th.testwebview;
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.net.http.SslError;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnKeyListener;
 import android.webkit.GeolocationPermissions;
 import android.webkit.SslErrorHandler;
 import android.webkit.WebChromeClient;
@@ -12,20 +17,36 @@ public class MainActivity extends Activity {
     // here you can put your url address
     // @BindView(R.id.webView)
     WebView webView;
+    String url = "https://165.22.240.130";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        webView = (WebView)findViewById(R.id.webView);
 
+        intentMethod();
+        webviewMethod();
+
+    }
+
+    public void intentMethod() {
+        //intent to browser
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(url));
+        startActivity(i);
+    }
+
+    public void webviewMethod() {
+        //webview
+        webView = (WebView)findViewById(R.id.webView);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setAppCacheEnabled(true);
         webView.getSettings().setDatabaseEnabled(true);
         webView.getSettings().setDomStorageEnabled(true);
-        webView.getSettings().setSupportZoom(true);
+//        webView.getSettings().setSupportZoom(true);
         webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
-        webView.getSettings().setBuiltInZoomControls(true);
+//        webView.getSettings().setBuiltInZoomControls(true);
         //webView.setWebViewClient(new GeoWebViewClient());
         // Below required for geolocation
 
@@ -46,5 +67,31 @@ public class MainActivity extends Activity {
             }
         });
 
+        webView.setOnKeyListener(new OnKeyListener()
+        {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event)
+            {
+                if(event.getAction() == KeyEvent.ACTION_DOWN)
+                {
+                    WebView webView = (WebView) v;
+
+                    switch(keyCode)
+                    {
+                        case KeyEvent.KEYCODE_BACK:
+                            if(webView.canGoBack())
+                            {
+                                webView.goBack();
+                                return true;
+                            }
+                            break;
+                    }
+                }
+
+                return false;
+            }
+        });
     }
+
+
 }
---
+++ TestWebView/app/src/main/java/pasagorn/saengsawang/kku/ac/th/testwebview/SplashActivity.java
@@ -0,0 +1,63 @@
+package pasagorn.saengsawang.kku.ac.th.testwebview;
+
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+public class SplashActivity extends AppCompatActivity {
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        Window window = getWindow();
+        window.setFormat(PixelFormat.RGB_888);
+    }
+
+    Thread splashTread;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        StartAnimation();
+    }
+
+    private void StartAnimation() {
+        Animation anim = AnimationUtils.loadAnimation(this,R.anim.alpha);
+        anim.reset();
+        LinearLayout l = (LinearLayout) findViewById(R.id.lin_lay);
+        l.clearAnimation();
+        l.startAnimation(anim);
+
+        anim = AnimationUtils.loadAnimation(this,R.anim.translate);
+        anim.reset();
+        ImageView iv = (ImageView) findViewById(R.id.logo);
+        iv.clearAnimation();
+        iv.startAnimation(anim);
+
+        splashTread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    int waited = 0;
+
+                    while (waited < 3500) {
+                        sleep(100);
+                        waited += 100;
+                    }
+                    Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                    startActivity(intent);
+                    SplashActivity.this.finish();
+                } catch (InterruptedException e) {
+
+                } finally {
+                    SplashActivity.this.finish();
+                }
+            }
+        };
+        splashTread.start();
+    }
+}
